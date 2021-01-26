3af3fc4aab4177c83970dfe0b6e36a1a43c51016
Alejandro Ayala <76751990+alejandroayalamx@users.noreply.github.com>
2021-01-25 20:19:40 -0600

    Add files via upload

---
+++ generador.php
@@ -0,0 +1,23 @@
+<?php
+	//obtener longitud de clave
+	$longitud = $_GET['longitud'];
+
+	//generador
+	function generador($longitud)
+	{
+		//caracteres a utilizar
+		$chars = "0AaBbCc1DdEe2FfGgHh3IiJj4KkLlMm5NnOo6PpQqRr7SsTt8UuVvWwXx9YyZz$";
+		//inicializar la clave
+		$clave = "";
+
+		for($i=0; $i<$longitud; $i++)
+		{
+			//generar numero aleatorio
+			$num = rand(1, strlen($chars));
+			//crear contraseña
+			$clave .= substr($chars, $num-1, 1);
+		}
+		return $clave;
+	}
+
+	echo generador($longitud);
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Generador</title>
+</head>
+<body>
+	<!--formulario para obtener la longitud de caracteres-->
+	<form method="GET" action="generador.php">
+		<input type="number" name="longitud" placeholder="Longitud de la clave"><br>
+		<input type="submit" value="Generar">
+	</form>
+</body>
+</html>
